0bf8665dc9e99ed117e0ee5709c5145cd2e07b3c
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 10:57:35 +0530

    Add:Reading values from Dictionary to Array

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo " welcome to Arithmetic computation program "
-declare -A compute
+declare -A compute                                    #storing compute in Dictionary
+declare -a valuesArr
+
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
@@ -12,3 +14,9 @@ compute2=`echo | awk -v var1=$a -v var2=$b -v var3=$c '{calculate=a*b+c} END {pr
 compute3=`echo | awk -v var1=$a -v var2=$b -v var3=$c '{calculate=c+a/b} END {print calculate}'`
 #calculating a%b+c
 compute4=`echo | awk -v var1=$a -v var2=$b -v var3=$c '{calculate=a%b+c} END {print calculate}'`
+
+for value in "${compute[@]}"                            #for loop for values in array
+do
+        valuesArr[$arrayIndex]=$value                   #storing in an array
+        arrayIndex=$(( $arrayIndex + 1 ))
+done
