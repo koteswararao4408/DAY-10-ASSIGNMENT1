018af9ab05b067a51e0ac7ed08acb87ab7382cc1
koteswararao <koteswararao.pinjala@gmail.com>
2020-12-24 10:40:30 +0530

    Add:Compute a*b+c

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,5 +5,6 @@ read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter a Third Number : " c
 #calculating a+b*c
-compute=`echo | awk -v var1=$a -v var2=$b -v var3=$c '{calculate=a+b*c} END {print calculate}'`
-
+compute1=`echo | awk -v var1=$a -v var2=$b -v var3=$c '{calculate=a+b*c} END {print calculate}'`
+#calculating a*b+c
+compute2=`echo | awk -v var1=$a -v var2=$b -v var3=$c '{calculate=a*b+c} END {print calculate}'`
